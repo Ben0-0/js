e25a08893ca11c9d3bfd0d44022fd03009858c9e
Ben0-0 <lehuynhthiennhanTRK@gmail.com>
2024-06-14 18:16:03 +0700

    0.5 script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function genprod(product) {
+    if(product.image){
+        product.image = "data:image/png;base 64" + product.image;
+    }
+    const markup = `
+    <div class="product-container" id="${product.name}">
+        <img class="product-image" src="${product.image}" alt="${product.name}">
+        <p class="sale">${product.sale ? `-${product.sale}%` : ""}</p>
+        <p class="tag">${product.tag ? product.tag : ""}</p>
 
+        <h3 class="product-name">${product.name}</h3>
+        <p class="product-desc">${product.desc}</p>
+        <h4 class="product-nprice">${product.currency} ${product.nprice}</h4>
+        <p class="product-oprice"><del>${product.oprice ? `${product.currency} ${product.oprice}` : ""}</del></p>
+        <div class="product-utility">
+            <button class="product-cart"> Add to Cart</button>
+            <a href=""></a>
+    `
+}
