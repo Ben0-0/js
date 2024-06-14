bb1e96140030158534187a3bc312df49b6872c8b
Ben0-0 <lehuynhthiennhanTRK@gmail.com>
2024-06-14 21:00:13 +0700

    script yippe

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let pformat = new Intl.NumberFormat('de-DE', { style: 'currency', currency:'IDR'}); //để format tiền tệ, vd: pformat.format(2500) sẽ output ra Rp 2.500,00 (I think so?)
+let pformat = new Intl.NumberFormat('de-DE', { style: 'currency', minimumSignificantDigits : 2 }); //để format tiền tệ, vd: pformat.format(2500) sẽ output ra 2.500,00 (I think so?)
+
+function capitalization(string) { //viết hoa chữ cái đầu tiên của string
+    return string.charAt(0).toUpperCase() + string.slice(1); //trả về string đó nhưng viết hoa chữ cái đầu tiên. ví dụ: capitalization(zxlckvn) thì sẽ trả về: kí tự đầu tiên của string(kí tự số 0 là z).toUpperCase sẽ biến nó thành Z, khi này string.slice(1) sẽ trả lại string ban đầu nhưng từ kí tự 1 trở đi, tức là trả xlckvn rồi cộng 2 cái lại sẽ ra Zxlckvn.
+}
 
 function genprod(product) {
     if(product.image){
         product.image = "data:image/png;base64," + product.image; //đổi sang format đúng để display ảnh sử dụng base 64, lưu ý là product.image kiểu dữ liệu ban đầu là base 64, dòng code này chỉ khiến cho nó trở thành đúng định dạng hiển thị trong js
     }
-    if(product.price){
-        product.price = pformat.format(product.price); //đổi cho đúng format display tiền tệ trên Furniro
+    if(product.old_price){
+        product.old_price = pformat.format(product.old_price); //same as below (line 17)
     }
+    if(product.tag){
+        product.tag = capitalization(product.tag); //capitalize cái tag cho đẹp
+    }
+    let product.short_desc = capitalization(product.short_desc); //capitalize chữ cái đầu tiên trong phần giới thiệu sơ về product
+    let product.name = capitalization(product.name); //capitalize chữ cái đầu của tên product
+    let product.unit_price = capitalization(product.unit_price); //capitalize chữ cái đầu của unit of currency (Usd, Vnd,...); theo design trên web Furniro thì nó sẽ là Rp
+    let product.price = pformat.format(product.price); //đổi cho đúng format display tiền tệ trên Furniro
+
     const markup = `
     <div class="product-container" id="${product.name}"> 
         <img class="product-image" src="${product.image}" alt="${product.name}">
@@ -15,7 +27,7 @@ function genprod(product) {
 
         <h3 class="product-name">${product.name}</h3>
         <p class="product-desc">${product.short_desc}</p>
-        <h4 class="product-price">${product.price}</h4>
+        <h4 class="product-price">${product.unit_price} ${product.price}</h4>
         <p class="product-old_price"><del>${product.old_price ? `${product.unit_price} ${product.old_price}` : ""}</del></p>
         <div class="product-utility">
             <button class="product-cart">Add to Cart</button>
